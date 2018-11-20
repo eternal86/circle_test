9349e1bedd5c4d56a7ed768a08d3345677935e1f
AndrewS <eternal86@gmail.com>
2018-11-20 10:30:39 +0100

    +

--- boards/tests.py
+++ boards/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class FirstTests(TestCase):
+    def test_1_e_1(self):
+        self.assertEquals(1, 1)
+
+class SecondTests(TestCase):
+    def test_2_e_2(self):
+        self.assertEquals(2, 2)
